27324a0f6b578f139d05c4978ae6bf66a6107f54
Khu Ûi-siông <uisiong.khu@gmail.com>
2022-04-07 23:03:21 +0800

    [修正] DUMMY_EXPESES 換行

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2",
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) },
   {
     id: "e3",
     title: "Car Insurance",
@@ -27,7 +30,7 @@ const App = () => {
   
   const addExpenseHandler = (expense: any) => {
     setExpenses((prevExpenses: any) => {
-      return [expense, ...prevExpenses];
+      return [expense, ...prevExpenses]; // ... 後就是要Append的東西
     });
   };
   return (
